a8b00b6764be6f728e01e3954c47bbdfe3443892
AlbertGabdullin <albert.gabdullin@flatstack.com>
2018-08-17 11:13:03 +0300

    responsive home page

--- app/js/common.js
+++ app/js/common.js
@@ -3,6 +3,26 @@ $(function () {
 
 	$('.useful-articles-items').owlCarousel({
 		items: 3,
+
+		responsive: {
+			0: {
+				items: 1,
+				dots: true,
+			},
+			768: {
+				items: 2,
+				dots: false,
+			},
+
+			1200: {
+				items: 3,
+			}
+		}
+	});
+
+	$('.menu-btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('menu-btn--active');
 	});
 
 	//SVG Fallback
@@ -11,6 +31,48 @@ $(function () {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
+
+	//function for match max height
+	$.fn.itemheight = function(){
+			var $blocks = $(this);
+			var maxH = $(this).height();
+			$($blocks).each(function(){
+					if($(this).height() > maxH){
+							maxH = $(this).height();
+					}
+			});
+			$blocks.height(maxH);
+	}
+
+
+	//slider main-section
+
+	if($(window).width() <= 992) {
+		$('.main-services').addClass('owl-carousel');
+		$('.main-services').owlCarousel({
+			items: 1,
+			dots: true
+		});
+
+		$('.counter-items').addClass('owl-carousel');
+		$('.counter-items').owlCarousel({
+			items: 1,
+			dots: true
+		});
+	}
+
+	if($(window).width() <= 768) {
+		$('.advantages-items').addClass('owl-carousel');
+		$('.advantages-items').owlCarousel({
+			items: 1,
+			dots: true
+		});
+	}
+
+
+	$('.show-advantages').on('click', function() {
+		$(this).siblings('.list').slideDown(300);
+	});
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
